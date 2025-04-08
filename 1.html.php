1cbfea893c3efe6f28bd613336cdc746a0e6b7f6
5tzzz <tzzzhen@gmail.com>
2025-04-08 10:11:46 +0800

    1.html.php

---
+++ 1.html.php
@@ -0,0 +1,13 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+    <?php
+//宣告變數 $name 
+    $name="陳亭蓁";
+    echo"<p>hello,$name!</p>";
+//時區台北 設定時間
+ date_defult_timezone_set("Asia/Taipei");
+ echo"<p>現在時間:. date("Y-m-d H:i:s")
+ 
+    </body>
+</html>
